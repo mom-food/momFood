d400994fd9f06c17ca3b16c8bea7f35d34f97f40
Jana-mousa <92536722+Jana-mousa@users.noreply.github.com>
2024-05-02 15:44:52 +0300

    Splash Screen

---
+++ app/lib/View/Screens/SplashScreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'Home.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToHome();
+  }
+
+  navigateToHome() async {
+    // You can replace this with your own duration
+    await Future.delayed(Duration(seconds: 3), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MyHomePage(title: 'Onboarding',)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // This sets the background color to orange
+      body: Center(
+        child: Image.asset(
+            'assets/images/Logo.png'), // This adds the logo to the center of the splash screen
+      ),
+    );
+  }
+}
