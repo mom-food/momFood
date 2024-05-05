78c9c14fdc8d2d0347b9508ccfccec91ef31fdf6
Hadeel <amrhadeel6@gmail.com>
2024-05-05 20:31:49 +0300

    update colors

--- app/lib/colors.dart
+++ app/lib/colors.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-    static const Color primaryLight= Color(0xFFF6BD60);
-    static const Color primaryDark= Color(0xFFECEDF0);
-    static const Color secondaryLight= Color(0xFFA8DF83);
-    static const Color secondaryDark = Color(0xFFD9D9D9);
-    static const Color textColorDark= Color(0xFF000000);
-    static const Color textColorLight= Color(0xFFFFFFFF);
+    static const Color primary1= Color(0xFFF6BD60);
+    static const Color primary2= Color(0xFFA8DF83);
+    static const Color secondary1 = Color(0xFFD9D9D9);
+    static const Color secondary2 = Color(0xFFD9D9D9);
+    static const Color secondary3 = Color(0xFFECEDFO);
+    static const Color textColor1= Color(0xFF000000);
+    static const Color textColor2= Color(0xFFFFFFFF);
     static const Color backgroundColorLight= Color(0xFFFFFFFF);
     static const Color backgroundColorDark= Color(0xFF000000);
     
