8ad0aaac67aa3889908de28ff634606310d98d1b
Jana-mousa <92536722+Jana-mousa@users.noreply.github.com>
2024-05-06 14:04:16 +0300

    add dot indicator

--- app/lib/View/Screens/OnBoarding1.dart
+++ app/lib/View/Screens/OnBoarding1.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
+import '../Widgets/PageIndicator.dart';
 import 'Home.dart';
 
 class Onboarding1 extends StatefulWidget {
@@ -38,7 +40,8 @@ class _Onboarding1State extends State<Onboarding1> {
                   style: TextStyle(fontSize: 32),
                 ),
               ),
-              SizedBox(height: 20), // Add space between "مرحباً" text and image
+              SizedBox(height: 15),
+              SizedBox(width: 15),
 
               Padding(
                 padding: const EdgeInsets.only(top: 50,left: 20),
@@ -50,41 +53,63 @@ class _Onboarding1State extends State<Onboarding1> {
                   ),
                 ),
               ),
-              SizedBox(height: 50),
+              SizedBox(height: 15),
+              SizedBox(width: 15),
               Padding(
                 padding: const EdgeInsets.only(top: 50),
-                child: RichText(
-                  text: TextSpan(
-                    text: 'اهلا ',
-                    style: TextStyle(fontSize: 26, color: Colors.black),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'بكم',
-                        style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
-                      ),
-                      TextSpan(text: ' في '),
-                      TextSpan(
-                        text: 'عالم',
-                        style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
-                      ),
-                      TextSpan(text: ' النكهات '),
-                      TextSpan(
-                        text: 'الاصيلة',
-                        style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
-                      ),
-                      TextSpan(text: ', حيث كل '),
-                      TextSpan(
-                        text: 'طبق',
-                        style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
-                      ),
-                      TextSpan(text: ' يروي '),
-                      TextSpan(
-                        text: 'قصته',
-                        style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
-                      ),
-                    ],
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: RichText(
+                    text: TextSpan(
+                      text: ' اهلا ',
+                      style: TextStyle(fontSize: 26, color: Colors.black),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'بكم',
+                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                        ),
+                        TextSpan(text: ' في '),
+                        TextSpan(
+                          text: 'عالم',
+                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                        ),
+                        TextSpan(text: ' النكهات '),
+                        TextSpan(
+                          text: 'الاصيلة',
+                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                        ),
+                        TextSpan(text: ', حيث كل '),
+                        TextSpan(
+                          text: 'طبق',
+                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                        ),
+                        TextSpan(text: ' يروي '),
+                        TextSpan(
+                          text: 'قصته',
+                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 15),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: PageIndicator(
+                    controller: _pageController,
+                    count: 2,
+                    onDotClicked: (index) {
+                      if (index == 1) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => MyHomePage(title: "Onboarding2")),
+                        );
+                      }
+                    },
+                  ),
                 ),
               ),
             ],
