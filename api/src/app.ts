41665661933b87d2eb7380c08ad3bf8561f31a0d
Hadeel <amrhadeel6@gmail.com>
2024-05-08 12:22:25 +0300

    create API's and create category crud operation s

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,14 +2,15 @@ import express from "express";
 import connectDB  from './database';
 import dotenv from "dotenv";
 import router from './routes';
-// Load environment variables from .env file
 dotenv.config()
 
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/', router);
+app.use(express.json());
+app.use('/api', router);
+app.use(express.static('src/public'))
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
