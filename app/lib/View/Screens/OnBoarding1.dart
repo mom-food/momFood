74ceea3d114b1aa7672baee6685494ea31957165
Jana-mousa <92536722+Jana-mousa@users.noreply.github.com>
2024-05-08 17:35:55 +0300

    Update Onboarding theam

--- app/lib/View/Screens/OnBoarding1.dart
+++ app/lib/View/Screens/OnBoarding1.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
+import '../../colors.dart';
 import '../Widgets/PageIndicator.dart';
 import 'Home.dart';
 
@@ -67,31 +68,52 @@ class _Onboarding1State extends State<Onboarding1> {
                   child: RichText(
                     text: TextSpan(
                       text: ' اهلا ',
-                      style: TextStyle(fontSize: 26, color: Colors.black),
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Theme.of(context).brightness == Brightness.dark ? AppColors.textColor2 : AppColors.textColor1,
+                      ),
                       children: <TextSpan>[
                         TextSpan(
                           text: 'بكم',
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).brightness == Brightness.dark ? AppColors.primary2 : AppColors.primary1,
+                          ),
                         ),
-                        TextSpan(text: ' في '),
+                        TextSpan(
+                          text: ' في ',
+
+                        ),
                         TextSpan(
                           text: 'عالم',
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).brightness == Brightness.dark ? AppColors.primary2 : AppColors.primary1,
+                          ),
                         ),
-                        TextSpan(text: ' النكهات '),
+                        TextSpan(text: ' النكهات ',),
                         TextSpan(
                           text: 'الاصيلة',
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).brightness == Brightness.dark ? AppColors.primary2 : AppColors.primary1,
+                          ),
                         ),
                         TextSpan(text: ', حيث كل '),
                         TextSpan(
                           text: 'طبق',
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).brightness == Brightness.dark ? AppColors.primary2 : AppColors.primary1,
+                          ),
                         ),
                         TextSpan(text: ' يروي '),
                         TextSpan(
                           text: 'قصته',
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFF6BD60)),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).brightness == Brightness.dark ? AppColors.primary2 : AppColors.primary1,
+                          ),
                         ),
                       ],
                     ),
