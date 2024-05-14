175eec3f086c800d9cdc124a9627eab1ebca53d7
Jana-mousa <92536722+Jana-mousa@users.noreply.github.com>
2024-05-15 00:46:40 +0300

    Create Widget OfferMeal

---
+++ app/lib/View/Screens/OfferMeals.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import '../Widgets/cards/meal_offer_card.dart';
+
+class MealOfferScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              'العروضات',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            MealOfferCard(
+              imagePath: 'assets/images/Onboarding1.png',
+              title: 'معجنات خبز',
+              originalPrice: '10',
+              discountedPrice: '55',
+              originalPriceIconPath: 'assets/images/icon-shekel.png',
+              discountedPriceIconPath: 'assets/images/icon-shekel.png',
+            ),
+            SizedBox(width: 20),
+            MealOfferCard(
+              imagePath: 'assets/images/Onboarding1.png',
+              title: 'معجنات خبز',
+              originalPrice: '10',
+              discountedPrice: '55',
+              originalPriceIconPath: 'assets/images/icon-shekel.png',
+              discountedPriceIconPath: 'assets/images/icon-shekel.png',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
