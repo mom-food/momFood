bd07370e3ba617b8e8db3c863a87fa7a374293ad
Jana-mousa <92536722+Jana-mousa@users.noreply.github.com>
2024-05-15 10:31:25 +0300

    Logic for MealDetails

---
+++ app/lib/Model/mealDetailsModel.dart
@@ -0,0 +1,9 @@
+class MealDetailsModel {
+  final String id;
+  final String name;
+  final String image;
+  final String description;
+  final double price;
+
+  MealDetailsModel ({required this.id, required this.name,required this.image, required this.description, required this.price});
+}
