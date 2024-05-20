5fb0d5979011b6b8caf51632651b2373a1fb1b1c
Jana-mousa <92536722+Jana-mousa@users.noreply.github.com>
2024-05-20 11:51:14 +0300

    Update connect firebase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,13 +1,43 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'Model/search-model.dart';
+import 'ViewModel/meal_view_model.dart';
+import 'View/Screens/Home.dart';
+import 'View/Screens/OnBoarding1.dart';
+import 'View/Screens/OnBoarding2.dart';
 import 'View/Screens/SplashScreen.dart';
-import 'package:app/View/Screens/OnBoarding1.dart';
+import 'View/Screens/offer.dart';
+import 'themes/theme-provider.dart';
 import 'themes/dark.dart';
 import 'themes/light.dart';
 import 'package:app/View/widgets/app_bar.dart';
-import 'package:app/View/widgets/nav_bar.dart';
-void main() {
-  runApp( MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    if (Platform.isAndroid) {
+      await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: "AIzaSyAh1_uBnxz3toNPno1uvm0knZwtOwSKrek",
+          appId: "1:175323531680:android:5cb7cd5c3878a57d47fcc5",
+          messagingSenderId: "175323531680",
+          projectId: "momfood-6ae63",
+        ),
+      );
+    } else {
+      await Firebase.initializeApp();
+    }
+    print("Firebase initialized successfully");
+  } catch (e) {
+    print("Error initializing Firebase: $e");
+  }
+  runApp(MyApp());
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -21,9 +51,10 @@ class _MyAppState extends State<MyApp> {
   void _toggleTheme() {
     setState(() {
       _themeMode =
-      _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+          _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
     });
   }
+
   int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
@@ -34,33 +65,44 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: _themeMode == ThemeMode.light ? ThemeData.light(): ThemeData.dark(),
-      home: Scaffold(
-        appBar: MyAppBar(
-
-          title: 'Mom Food',
-          isLightTheme: _themeMode == ThemeMode.light,
-        ),
-        body: SplashScreen(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _toggleTheme,
-          tooltip: 'Switch',
-          child: Icon(
-            _themeMode == ThemeMode.light ? Icons.wb_sunny: Icons.nightlight_round,
-            color: _themeMode == ThemeMode.light
-                ? DarkThemeData.dark.colorScheme.secondary
-                : LightThemeData.light.colorScheme.secondary,
-          ),
-
-          ),
-
-        backgroundColor: _themeMode == ThemeMode.light
-            ? DarkThemeData.dark.colorScheme.background
-            : LightThemeData.light.colorScheme.background,
-        ),
-
+    return MultiProvider(
+      providers: [
+        //ChangeNotifierProvider(create: (_) => MomFood()),
+        ChangeNotifierProvider(create: (_) => MealViewModel()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            themeMode:
+                themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+            theme: themeProvider.isDarkMode ? darkMode : lightMode,
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: MyAppBar(
+                title: 'Mom Food',
+                isLightTheme: !themeProvider.isDarkMode,
+              ),
+              body: SplashScreen(),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  themeProvider.toggleTheme();
+                },
+                tooltip: 'Switch',
+                child: Icon(
+                  themeProvider.getThemeIcon(),
+                ),
+              ),
+            ),
+            routes: {
+              '/onboarding1': (context) => Onboarding1(),
+              '/onboarding2': (context) => Onboarding2Screen(),
+              '/offer': (context) => HomePage(),
+            },
+          );
+        },
+      ),
     );
   }
 }
