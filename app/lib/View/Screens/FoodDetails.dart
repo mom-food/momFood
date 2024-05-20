8c8521cbb1389160413afd074295c66e6e45d59d
tabarkismael <tabarrk.ismael@gmail.com>
2024-05-20 19:04:34 +0300

    edit food details to took data by id

--- app/lib/View/Screens/FoodDetails.dart
+++ app/lib/View/Screens/FoodDetails.dart
@@ -5,8 +5,10 @@ import '../../Model/mealDetailsModel.dart';
 import '../../colors.dart';
 import 'Cart.dart';
 class FoodDetailsPage extends StatelessWidget {
+  final String mealId;
   final Cart cart;
-  FoodDetailsPage(this.cart);
+
+  FoodDetailsPage(this.cart,this.mealId);
   @override
   Widget build(BuildContext context) {
     MealDetailsServices mealDetailsServices = new MealDetailsServices();
@@ -23,7 +25,7 @@ class FoodDetailsPage extends StatelessWidget {
         ],
       ),
       body: FutureBuilder<MealDetailsModel?>(
-        future: mealDetailsServices.fetchMealDetails('662978ed6ea592baeb0652ba'),
+        future: mealDetailsServices.fetchMealDetails(mealId),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
