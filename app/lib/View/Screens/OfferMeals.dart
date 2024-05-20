4e77bbd88b5452bb4296b4bd73538ad4bc243843
Hadeel <amrhadeel6@gmail.com>
2024-05-20 12:40:46 +0300

    Revert "Merge pull request #16 from hadeelamr/meal-management"
    This reverts commit 04f385ac1fa53a2acf0084ae3a46a5a97513d9f8, reversing
    changes made to 023c41c0fab246f9e376b1e4ba596e3e21d148b8.

--- app/lib/View/Screens/OfferMeals.dart
+++ app/lib/View/Screens/OfferMeals.dart
@@ -1,6 +1,55 @@
+import 'package:flutter/material.dart';
+import '../Widgets/cards/meal_offer_card.dart';
+
+class MealOfferScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              'العروضات',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+              ),
+            ),
+          ],
+        ),
+        automaticallyImplyLeading: false, // هذا السطر يحذف زر العودة
+      ),
+      body: SingleChildScrollView(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            MealOfferCard(
+              imagePath: 'assets/images/Onboarding1.png',
+              title: 'معجنات خبز',
+              originalPrice: '10',
+              discountedPrice: '55',
+              originalPriceIconPath: 'assets/images/icon-shekel.png',
+              discountedPriceIconPath: 'assets/images/icon-shekel.png',
+            ),
+            SizedBox(width: 20),
+            MealOfferCard(
+              imagePath: 'assets/images/Onboarding1.png',
+              title: 'معجنات خبز',
+              originalPrice: '10',
+              discountedPrice: '55',
+              originalPriceIconPath: 'assets/images/icon-shekel.png',
+              discountedPriceIconPath: 'assets/images/icon-shekel.png',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 // import 'package:flutter/material.dart';
 // import '../Widgets/cards/meal_offer_card.dart';
-
+//
 // class MealOfferScreen extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
@@ -18,7 +67,6 @@
 //             ),
 //           ],
 //         ),
-//         automaticallyImplyLeading: false, // هذا السطر يحذف زر العودة
 //       ),
 //       body: SingleChildScrollView(
 //         child: Row(
@@ -47,51 +95,3 @@
 //     );
 //   }
 // }
-// // import 'package:flutter/material.dart';
-// // import '../Widgets/cards/meal_offer_card.dart';
-// //
-// // class MealOfferScreen extends StatelessWidget {
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       appBar: AppBar(
-// //         title: Row(
-// //           mainAxisAlignment: MainAxisAlignment.end,
-// //           children: <Widget>[
-// //             Text(
-// //               'العروضات',
-// //               style: TextStyle(
-// //                 fontWeight: FontWeight.bold,
-// //                 fontSize: 26,
-// //               ),
-// //             ),
-// //           ],
-// //         ),
-// //       ),
-// //       body: SingleChildScrollView(
-// //         child: Row(
-// //           mainAxisAlignment: MainAxisAlignment.end,
-// //           children: [
-// //             MealOfferCard(
-// //               imagePath: 'assets/images/Onboarding1.png',
-// //               title: 'معجنات خبز',
-// //               originalPrice: '10',
-// //               discountedPrice: '55',
-// //               originalPriceIconPath: 'assets/images/icon-shekel.png',
-// //               discountedPriceIconPath: 'assets/images/icon-shekel.png',
-// //             ),
-// //             SizedBox(width: 20),
-// //             MealOfferCard(
-// //               imagePath: 'assets/images/Onboarding1.png',
-// //               title: 'معجنات خبز',
-// //               originalPrice: '10',
-// //               discountedPrice: '55',
-// //               originalPriceIconPath: 'assets/images/icon-shekel.png',
-// //               discountedPriceIconPath: 'assets/images/icon-shekel.png',
-// //             ),
-// //           ],
-// //         ),
-// //       ),
-// //     );
-// //   }
-// // }
