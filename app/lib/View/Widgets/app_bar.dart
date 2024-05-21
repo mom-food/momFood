3e06e06f5e401815479886ff55f6161a8f42641d
Hadeel <amrhadeel6@gmail.com>
2024-05-21 11:09:45 +0300

    resolve conflicts to merge menue list

--- app/lib/View/Widgets/app_bar.dart
+++ app/lib/View/Widgets/app_bar.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../../themes/theme-provider.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final String title;
+  final dynamic title;
   final bool isLightTheme;
 
   const MyAppBar({
     Key? key,
     required this.title,
     required this.isLightTheme,
-
   }) : super(key: key);
 
   @override
@@ -18,27 +15,23 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
-      return AppBar(
-        title: _buildTitleWidget(),
-        centerTitle: true,
-        backgroundColor:
-        themeProvider.isDarkMode ? Colors.black : Colors.white,
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            icon: Icon(
-              themeProvider.getThemeIcon(),
-              color: themeProvider.isDarkMode ? Colors.white : Colors.black,
-            ),
-            onPressed: () {
-              themeProvider.toggleTheme();
-            },
-          ),
-        ],
-      );
-        },
+    return AppBar(
+      title: _buildTitleWidget(),
+      centerTitle: true,
+      backgroundColor: isLightTheme ? Colors.white : Colors.black,
+      automaticallyImplyLeading: false,
     );
+  }
+
+  Widget _buildTitleWidget() {
+    if (title is String) {
+      return Text(
+        title,
+      );
+    } else if (title is Widget) {
+      return title;
+    } else {
+      return Container();
+    }
   }
 }
