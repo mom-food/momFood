dbe6cd93894c2e30c74adc35be5a959ac985fc4f
Hadeel <amrhadeel6@gmail.com>
2024-05-22 17:59:20 +0300

    update

--- app/lib/View/Screens/HomePage.dart
+++ app/lib/View/Screens/HomePage.dart
@@ -1,10 +1,54 @@
-import 'package:app/View/Screens/temporary_cart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import '../../ViewModel/meal_view_model.dart';
+import '../../Model/meal_model.dart';
+import '../Widgets/search-bar.dart';
+import 'package:app/View/Screens/temporary_cart.dart';
 import '../../themes/theme-provider.dart';
 import 'CategoryMeal.dart';
 import 'OfferMeals.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mom FOod'),
+      ),
+      body: Column(
+        children: [
+          CustomSearchBar(onSearch: (query) {
+            Provider.of<MealViewModel>(context, listen: false).search(query);
+          }),
+          Expanded(
+            child: Consumer<MealViewModel>(
+              builder: (context, mealViewModel, child) {
+                if (mealViewModel.meals.isEmpty) {
+                  return Center(
+                    child: Text('No meals found.'),
+                  );
+                } else {
+                  return ListView.builder(
+                    itemCount: mealViewModel.meals.length,
+                    itemBuilder: (context, index) {
+                      Meal meal = mealViewModel.meals[index];
+                      return ListTile(
+                        title: Text(meal.name),
+                        subtitle: Text(meal.description),
+                        leading: Image.network(meal.image),
+                        trailing: Text('\$${meal.price.toString()}'),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class CustomBottomNavBar extends StatelessWidget {
   final int selectedIndex;
