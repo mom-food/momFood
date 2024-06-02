45066d2496fdf906bc40261a6a6c18bec4d666a9
Hadeel <amrhadeel6@gmail.com>
2024-06-02 23:04:14 +0300

    update homepage

--- app/lib/View/Widgets/search-bar.dart
+++ app/lib/View/Widgets/search-bar.dart
@@ -12,37 +12,109 @@ class CustomSearchBar extends StatefulWidget {
 }
 
 class _CustomSearchBarState extends State<CustomSearchBar> {
-  late SearchController _controller;
+  late TextEditingController _controller;
   bool isDarkMode = false;
   List<String> searchHistory = [];
 
   @override
   void initState() {
     super.initState();
-    _controller = SearchController();
+    _controller = TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-            child: Consumer<MealViewModel>(builder: (context, momFood, child) {
-          return TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-              hintText: 'البحث',
-              suffixIcon: IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {
-                  String query = _controller.text.trim();
-                  momFood.search(query);
-                },
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Container(
+            width: 300,
+            height: 36,
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  child: Container(
+                    width: 300,
+                    height: 36,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x11000000),
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
+                          spreadRadius: 0,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 36,
+                          padding: const EdgeInsets.all(8),
+                          decoration: ShapeDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment(-1.00, -0.00),
+                              end: Alignment(1, 0),
+                              colors: [Color(0xFFE8E8E8), Colors.white],
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 28,
+                                height: 28,
+                                child: Icon(Icons.search, color: Color(0x993C3C43), size: 18),
+                              ),
+                              Expanded(
+                                child: Consumer<MealViewModel>(
+                                  builder: (context, momFood, child) {
+                                    return TextField(
+                                      controller: _controller,
+                                      decoration: InputDecoration(
+                                        hintText: 'البحث',
+                                        border: InputBorder.none,
+                                        isDense: true,
+                                        contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+                                        hintStyle: TextStyle(
+                                          color: Color(0x993C3C43),
+                                          fontSize: 12,
+                                          fontFamily: 'Encode Sans',
+                                          fontWeight: FontWeight.w400,
+                                          height: 0.17,
+                                        ),
+                                      ),
+                                      textAlign: TextAlign.right,
+                                      onSubmitted: (query) {
+                                        momFood.search(query);
+                                      },
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          );
-        })),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -57,6 +129,5 @@ extension ThemeDataExtension on ThemeData {
       brightness: themeData.brightness,
       colorScheme: themeData.colorScheme,
     );
-
   }
 }
