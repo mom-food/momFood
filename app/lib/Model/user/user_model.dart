2c49c9e03a4706422afdd5ffd9c6fd4cbe8ddd28
Jana-mousa <92536722+Jana-mousa@users.noreply.github.com>
2024-06-05 09:55:58 +0300

    Revert "update user servisec and model"
    This reverts commit 38504a71e97e7d1ff6404d5036932740e3004b0d.

--- app/lib/Model/user/user_model.dart
+++ app/lib/Model/user/user_model.dart
@@ -1,13 +1,12 @@
 import 'package:app/Model/order_model.dart';
 
 class CreateUserRequestBody{
-  String? id;
   String? email;
   String? name;
   String? phone;
   String? password;
   List<OrderModel>? orderHistory;
-  CreateUserRequestBody( {this.id, required this.email, required this.name, required this.password, required this.phone,this.orderHistory=const []});
+  CreateUserRequestBody( {required this.email, required this.name, required this.password, required this.phone,this.orderHistory=const []});
 
   Map<String,dynamic> toJson()=>{
     'email' :email,
